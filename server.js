d1486c4b9ce5ebd804c26d21938911e38695f9b7
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-02-05 07:59:05 +0500

    connecting our database using knex

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection : {
+        host:'127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'fast',
+        database: 'smart-brain'
+    }
+})
+
+
 
 /*
 res = this is working
